bbbd5cc9fe996e8bce2b83b29e33d8103657e0a2
aswnss <aswnss.m@gmail.com>
2022-09-17 20:16:52 +0530

    navbar-css

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import markdownLogo from "./assets/markdown.png"
+import documentLogo from "./assets/document.png"
+import "./assets/Navbar.css"
 export default function Navbar(){
     return(
         <div className="navbar">
-            <img src={markdownLogo} alt="MarkdownLogo" />
-            <h1>Markdown Creator</h1>
+            <img src={documentLogo} alt="Document Logo" />
+            <h1>Document Creator</h1>
         </div>
     )
 }
