d93c62b1bc13b213c4e2fa5d05aa27c141d671d5
aswnss <aswnss.m@gmail.com>
2022-09-17 19:45:06 +0530

    Fucntion is working

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-
+import { saveAs } from "file-saver"
+import { Document , Paragraph, Packer,TextRun, HeadingLevel}  from "docx";
 export default function Form() {
     const handleClick = (event) => {
         event.preventDefault();
+    
         const eventJSON = {
             "eventName" :  event.target[0].value,
             "eventAbstract" :event.target[1].value,
@@ -10,6 +12,61 @@ export default function Form() {
             "eventMode"  :event.target[3].value?"Online":"Offline",
             "eventAuthor" :event.target[5].value,
         }
+
+       const doc = new Document({
+        sections : [{
+            children : [
+                new Paragraph({
+                    text: eventJSON.eventName,
+                    heading: HeadingLevel.TITLE,
+                    break:1
+                }),
+                new Paragraph({
+                    text: "Abstract",
+                    heading : HeadingLevel.HEADING_3,
+                    bold: true,
+                }),
+                new Paragraph({
+                    text: eventJSON.eventAbstract,
+                    break : 1
+                }),
+                new Paragraph({
+                    text: "Details",
+                    heading: HeadingLevel.HEADING_3,
+                    bold : true
+                }),
+                new Paragraph({
+                  text : eventJSON.eventDetail,
+                  break : 1  
+                }),
+                new Paragraph({
+                    children: [
+                        new TextRun({
+                            text : "Mode : "
+                        }),
+                        new TextRun({
+                            text : eventJSON.eventMode,
+                            break : 1
+                        })
+                    ]
+                }),
+                new Paragraph({
+                    children : [
+                        new TextRun({
+                            text : "Author : "
+                        }),
+                        new TextRun({
+                            text : eventJSON.eventAuthor,
+                            break: 1
+                        })
+                    ]
+                })
+            ]
+        }]
+       })
+        Packer.toBlob(doc).then((blob) => {
+            saveAs(blob, "example.docx");
+        });       
         
     }
     return(
@@ -35,9 +92,9 @@ export default function Form() {
                     <td>
                         Detail :
                     </td>
-                    <tr>
+                    <td>
                         <textarea name="eventDetail" id="eventDetail" cols="30" rows="10" placeholder="Explain What will be covered in the event, What all will you be doing"></textarea>
-                    </tr>
+                    </td>
                 </tr>
                 <tr>
                     <td>
@@ -56,9 +113,10 @@ export default function Form() {
                         <input type="text" name="eventAuthor" id="eventAuthor" placeholder="Your Name" required/>
                     </td>
                 </tr>
-                <tr colspan="2">
-                    {/* <button onClick={handleClick}>Submit</button> */}
-                    <input type="submit" value="Submit" />
+                <tr colSpan="2">
+                    <td>
+                        <input type="submit" value="Submit" />
+                    </td>
                 </tr>
             </table>
         </form>
