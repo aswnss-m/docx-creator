e5125ab50a6effe9a360f57f43e116532b9d02f6
aswnss <aswnss.m@gmail.com>
2022-09-18 06:48:46 +0530

    input width changed

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { saveAs } from "file-saver"
 import { Document , Paragraph, Packer,TextRun, HeadingLevel}  from "docx";
+import "./assets/Form.css"
 export default function Form() {
     const handleClick = (event) => {
         event.preventDefault();
@@ -70,55 +71,57 @@ export default function Form() {
         
     }
     return(
-        <form action="/" onSubmit={handleClick}>
-            <table>
-                <tr>
-                    <td>
-                        Event Name :
-                    </td>
-                    <td>
-                        <input type="text" name="eventName" id="eventName" placeholder="Main Event Name" required/>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Abstract :
-                    </td>
-                    <td>
-                        <input type="text" name="eventAbstract" id="eventAbstract" placeholder="Short info about the event" required/>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Detail :
-                    </td>
-                    <td>
-                        <textarea name="eventDetail" id="eventDetail" cols="30" rows="10" placeholder="Explain What will be covered in the event, What all will you be doing"></textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Mode :
-                    </td>
-                    <td>
-                        Online : <input type="radio" name="eventMode" id="eventMode" value="Online"/>
-                        Offline : <input type="radio" name="eventMode" id="eventMode" value="Offline"/>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Name :
-                    </td>
-                    <td>
-                        <input type="text" name="eventAuthor" id="eventAuthor" placeholder="Your Name" required/>
-                    </td>
-                </tr>
-                <tr colSpan="2">
-                    <td>
-                        <input type="submit" value="Submit" />
-                    </td>
-                </tr>
-            </table>
-        </form>
+        <div className="form">
+            <form action="/" onSubmit={handleClick}>
+                <table>
+                    <tr>
+                        <td>
+                            Event Name :
+                        </td>
+                        <td>
+                            <input type="text" name="eventName" id="eventName" placeholder="Main Event Name" required/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Abstract :
+                        </td>
+                        <td>
+                            <input type="text" name="eventAbstract" id="eventAbstract" placeholder="Short info about the event" required/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Detail :
+                        </td>
+                        <td>
+                            <textarea name="eventDetail" id="eventDetail" cols="30" rows="10" placeholder="Explain What will be covered in the event, What all will you be doing"></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Mode :
+                        </td>
+                        <td>
+                            Online : <input type="radio" name="eventMode" id="eventMode" value="Online"/>
+                            Offline : <input type="radio" name="eventMode" id="eventMode" value="Offline"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Name :
+                        </td>
+                        <td>
+                            <input type="text" name="eventAuthor" id="eventAuthor" placeholder="Your Name" required/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}>
+                            <input type="submit" value="Submit" />
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
     )
 }
